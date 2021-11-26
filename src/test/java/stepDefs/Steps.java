39e9bcb1d5ff5b0db283c63398e928f771b8ec30
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-11-26 20:15:32 +0530

    Added cucumber.properties and step def

---
+++ src/test/java/stepDefs/Steps.java
@@ -0,0 +1,69 @@
+package stepDefs;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Steps {
+
+    @Given("user is on Home Page")
+    public void user_is_on_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user searches for {string}")
+    public void user_searches_for(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("choose to buy the first item from the search results")
+    public void choose_to_buy_the_first_item_from_the_search_results() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("selects the color as {string}")
+    public void selects_the_color_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("selects the size as {string}")
+    public void selects_the_size_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("clicks on the {string} button")
+    public void clicks_on_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("enters valid personal details on checkout page")
+    public void enters_valid_personal_details_on_checkout_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("accepts the t&c checkbox")
+    public void accepts_the_t_c_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("clicks on {string} button")
+    public void clicks_on_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("successful order confirmation message {string} is shown")
+    public void successful_order_confirmation_message_is_shown(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("an order number is displayed")
+    public void an_order_number_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
